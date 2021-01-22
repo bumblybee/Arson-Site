8bd7549a4bb13af30ee8382dc0d44523b1182de6
Tiffani Hess <hesstjune@gmail.com>
2021-01-22 14:40:40 -0600

    updated bootstrap and jquery scripts due to security restraints

--- app.js
+++ app.js
@@ -22,13 +22,13 @@ const port = process.env.PORT || 5000;
 
 app.set("view engine", "ejs");
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(cookieParser());
 app.use(compression());
-app.use(helmet());
+app.use(helmet.frameguard({ action: "SAMEORIGIN" }));
 
 // ----Logging----
 
