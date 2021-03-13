7f2fdc221f0d0e8847cf5bcfc48f44897936ea5c
Tiffani Hess <hesstjune@gmail.com>
2021-03-13 13:52:54 -0600

    additional cookie config for production

--- config/cookieConfig.js
+++ config/cookieConfig.js
@@ -1,4 +1,12 @@
+const cookieEnvConfig = {};
+
+if (process.env.NODE_ENV === "production") {
+  cookieEnvConfig.secure = true;
+  cookieEnvConfig.sameSite = "none";
+}
+
 module.exports = {
   httpOnly: true,
   maxAge: 3600000,
+  ...cookieEnvConfig,
 };
