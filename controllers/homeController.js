db100822a7a3054f4654704129e4d8c32e309552
Tiffani Hess <hesstjune@gmail.com>
2021-03-12 18:11:32 -0600

    get news and recipes that aren't marked deleted at homeController,getHome

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -5,8 +5,8 @@ const authService = require("../services/authService");
 
 exports.getHome = async (req, res) => {
   const { auth } = authService.checkAuth(req.cookies["_PAS"]);
-  let recipes = await Recipe.find().sort({ date: -1 });
-  let news = await News.find().sort({ date: -1 });
+  let recipes = await Recipe.find({ isDeleted: false }).sort({ date: -1 });
+  let news = await News.find({ isDeleted: false }).sort({ date: -1 });
   recipes = recipes.filter((recipe, index) => index < 2);
   news = news.filter((news, index) => index < 1)[0];
 
